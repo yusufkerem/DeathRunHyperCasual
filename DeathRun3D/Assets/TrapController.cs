f544718acdc62f9cc7708d9a238db91f8543e780
Yusuf Kerem <47509437+yusufkerem@users.noreply.github.com>
2021-02-28 14:13:07 +0300

    Pillar Trap Added

---
+++ DeathRun3D/Assets/TrapController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator pillarAnim;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        TrapTrigger();
+    }
+
+    void TrapTrigger()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pillarAnim.SetTrigger("isTriggered");
+        }
+    }
+}
