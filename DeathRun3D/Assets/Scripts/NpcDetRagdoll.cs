9ded685b313ea34fb3492a7e7f36065e494bf346
karabay06 <ibrahim.karabay.95@gmail.com>
2021-02-28 21:44:57 +0300

    Ragdoll Added

---
+++ DeathRun3D/Assets/Scripts/NpcDetRagdoll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDetRagdoll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Npc"))
+        {
+            Debug.Log("Tetiklendi");
+            collision.gameObject.GetComponent<NpcAi>().DoRagdoll();
+        }
+    }
+}
