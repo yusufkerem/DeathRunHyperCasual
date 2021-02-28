6be65947f97dfb1abec0c4bc2277b6ae9217bfbc
Yusuf Kerem <47509437+yusufkerem@users.noreply.github.com>
2021-02-28 13:34:23 +0300

    Npc Ai added
    Npc Ai System added

---
+++ DeathRun3D/Assets/NpcAi.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcAi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movementSpeed;
+    public bool collisionStatus;
+    public Animator anim;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        movementSpeed = Random.Range(1f, 2.2f);
+    }
+    void Update()
+    {
+        Movement(collisionStatus);
+    }
+
+    void Movement(bool collisionStatus)
+    {
+        if (!collisionStatus)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+}
---
+++ DeathRun3D/Assets/ObstacleDetector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDetector : MonoBehaviour
+{
+    private NpcAi npc;
+    private void Start()
+    {
+        npc = gameObject.transform.parent.gameObject.GetComponent<NpcAi>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag=="obstacle")
+        {
+            npc.collisionStatus = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "obstacle")
+        {
+            npc.collisionStatus = false;
+        }
+    }
+
+}
