1d7533f56d8fcb4781450cb76f66dddbb483340f
karabay06 <ibrahim.karabay.95@gmail.com>
2021-03-05 21:33:44 +0300

    TrapControl

--- DeathRun3D/Assets/Scripts/TrapController.cs
+++ DeathRun3D/Assets/Scripts/TrapController.cs
@@ -25,10 +25,23 @@ public class TrapController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            PillarTrap();
-            SpikeTrap();
-            SpinnerTrap();
-            BarrelTrap();
+            //
+            if (FindObjectOfType<NpcGroupMovement>().TrapSelector().name== "PillarTrapParent")
+            {
+                PillarTrap();
+            }
+            else if (FindObjectOfType<NpcGroupMovement>().TrapSelector().name == "BarrellTrapGenel")
+            {
+                BarrelTrap();
+            }
+            else if (FindObjectOfType<NpcGroupMovement>().TrapSelector().name == "Spinner")
+            {
+                SpinnerTrap();
+                
+            }
+
+            //SpikeTrap();
+
             StartCoroutine("BoxTrapp");
         }
     }
@@ -40,6 +53,7 @@ public class TrapController : MonoBehaviour
         {
             if (useCount==2)
             {
+                
                 pillarAnim.SetTrigger("isTriggered");
                 pillarTrap.transform.Find("Usage1").gameObject.GetComponent<Renderer>().material.SetColor("_Color",Color.red);
                 useCount--;
@@ -66,8 +80,19 @@ public class TrapController : MonoBehaviour
 
     public void BarrelTrap()
     {
-        barrelAnim.SetTrigger("isTriggered");
-        barrelAnim.gameObject.transform.parent.Find("varil").GetComponent<Rigidbody>().AddForce(Vector3.right * 300, ForceMode.Impulse);
+        foreach (var item in GameObject.Find("BarrellTrapGenel").GetComponentsInChildren<Animator>())
+        {
+            item.SetTrigger("isTriggered");
+        }
+        foreach (var item in GameObject.Find("BarrellTrapGenel").GetComponentsInChildren<Rigidbody>())
+        {
+            item.AddForce(Vector3.right * 300, ForceMode.Impulse);
+        }
+        //barrelAnim.SetTrigger("isTriggered");
+        //barrelAnim.gameObject.transform.parent.Find("varil").GetComponent<Rigidbody>().AddForce(Vector3.right * 300, ForceMode.Impulse);
+        //barrelAnim.gameObject.transform.parent.parent.Find("BarrelTrap").transform.GetChild(0).gameObject.GetComponent<Animator>().SetTrigger("isTriggered");
+        //barrelAnim.gameObject.transform.parent.parent.Find("BarrelTrap").Find("varil2").GetComponent<Rigidbody>().AddForce(Vector3.right * 300, ForceMode.Impulse);
+
     }
 
     public void BoxTrap()
